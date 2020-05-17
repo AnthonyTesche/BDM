f0dd52c5890a935540a36ae5241450eb25f39a7d
Anthony Tesche <tescheanthony@gmail.com>
2020-05-16 23:21:20 -0300

    :bug: Corrigido log onde não mantinha logs anteriores

--- app/BDM/helper/Logger.php
+++ app/BDM/helper/Logger.php
@@ -7,8 +7,8 @@ class Logger{
     public function log($text, $file){
         date_default_timezone_set('America/Sao_Paulo');
         if(file_exists('../var/' . $file)){
-            $archive = fopen('../var/' . $file, 'w');
-            $info = date("Y-m-d") . "T" . date("h:i:s") . ": " . $text;
+            $archive = fopen('../var/' . $file, 'a+');
+            $info = date("Y-m-d") . "T" . date("h:i:s") . ": " . $text ."\n";
             fwrite($archive, $info);
             fclose($archive);
         } else {
